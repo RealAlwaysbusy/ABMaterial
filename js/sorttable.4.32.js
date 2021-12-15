b4e3b3d8a4e2080fa2fa2819e70580f8dcec8fd3
Alain Bailleul <alain.bailleul@pandora.be>
2021-12-15 14:44:23 +0100

    Add files via upload

--- js/sorttable.4.32.js
+++ js/sorttable.4.32.js
@@ -1,4 +1,162 @@
+/*
 function dean_addEvent(t,e,r){if(t.addEventListener)t.addEventListener(e,r,!1);else{r.$$guid||(r.$$guid=dean_addEvent.guid++),t.events||(t.events={});var o=t.events[e];o||(o=t.events[e]={},t["on"+e]&&(o[0]=t["on"+e])),o[r.$$guid]=r,t["on"+e]=handleEvent}}function removeEvent(t,e,r){t.removeEventListener?t.removeEventListener(e,r,!1):t.events&&t.events[e]&&delete t.events[e][r.$$guid]}function handleEvent(t){var e=!0;t=t||fixEvent(((this.ownerDocument||this.document||this).parentWindow||window).event);var r=this.events[t.type];for(var o in r)this.$$handleEvent=r[o],!1===this.$$handleEvent(t)&&(e=!1);return e}function fixEvent(t){return t.preventDefault=fixEvent.preventDefault,t.stopPropagation=fixEvent.stopPropagation,t}var stIsIE=!1,isscr=!1;if(sorttable={init:function(){arguments.callee.done||(arguments.callee.done=!0,_timer&&clearInterval(_timer),document.createElement&&document.getElementsByTagName&&(sorttable.DATE_RE=/^(\d\d?)[\/\.-](\d\d?)[\/\.-]((\d\d)?\d\d)/,forEach(document.getElementsByTagName("table"),function(t){-1!=t.className.search(/\bsortable\b/)&&sorttable.makeSortable(t)})))},makeSortable:function(t,r){if(isscr=null!=r,0==t.getElementsByTagName("thead").length&&(the=document.createElement("thead"),the.appendChild(t.rows[0]),t.insertBefore(the,t.firstChild)),null==t.tHead&&(t.tHead=t.getElementsByTagName("thead")[0]),1==t.tHead.rows.length){sortbottomrows=[];for(var e=0;e<t.rows.length;e++)-1!=t.rows[e].className.search(/\bsortbottom\b/)&&(sortbottomrows[sortbottomrows.length]=t.rows[e]);if(sortbottomrows){null==t.tFoot&&(tfo=document.createElement("tfoot"),t.appendChild(tfo));for(e=0;e<sortbottomrows.length;e++)tfo.appendChild(sortbottomrows[e]);delete sortbottomrows}headrow=t.tHead.rows[0].cells;for(e=0;e<headrow.length;e++)headrow[e].className.match(/\bsorttable_nosort\b/)||(mtch=headrow[e].className.match(/\bsorttable_([a-z0-9]+)\b/),mtch&&(override=mtch[1]),mtch&&"function"==typeof sorttable["sort_"+override]?headrow[e].sorttable_sortfunction=sorttable["sort_"+override]:headrow[e].sorttable_sortfunction=sorttable.guessType(t,e,r),headrow[e].sorttable_columnindex=e,headrow[e].sorttable_tbody=null!=r?r.tBodies[0]:t.tBodies[0],dean_addEvent(headrow[e],"click",sorttable.innerSortFunction=function(t){if(-1!=this.className.search(/\bsorttable_sorted\b/))return null!=r&&sorttable.reverse(this.sorttable_tbody),this.className=this.className.replace("sorttable_sorted","sorttable_sorted_reverse"),this.removeChild(document.getElementById("sorttable_sortfwdind")),sortrevind=document.createElement("span"),sortrevind.id="sorttable_sortrevind",sortrevind.innerHTML=stIsIE?'&nbsp<font face="webdings">5</font>':"&nbsp;&#x25B4;",void this.appendChild(sortrevind);if(-1!=this.className.search(/\bsorttable_sorted_reverse\b/))return null!=r&&sorttable.reverse(this.sorttable_tbody),this.className=this.className.replace("sorttable_sorted_reverse","sorttable_sorted"),this.removeChild(document.getElementById("sorttable_sortrevind")),sortfwdind=document.createElement("span"),sortfwdind.id="sorttable_sortfwdind",sortfwdind.innerHTML=stIsIE?'&nbsp<font face="webdings">6</font>':"&nbsp;&#x25BE;",void this.appendChild(sortfwdind);if(theadrow=this.parentNode,forEach(theadrow.childNodes,function(t){1==t.nodeType&&(t.className=t.className.replace("sorttable_sorted_reverse",""),t.className=t.className.replace("sorttable_sorted",""))}),sortfwdind=document.getElementById("sorttable_sortfwdind"),sortfwdind&&sortfwdind.parentNode.removeChild(sortfwdind),sortrevind=document.getElementById("sorttable_sortrevind"),sortrevind&&sortrevind.parentNode.removeChild(sortrevind),this.className+=" sorttable_sorted",sortfwdind=document.createElement("span"),sortfwdind.id="sorttable_sortfwdind",sortfwdind.innerHTML=stIsIE?'&nbsp<font face="webdings">6</font>':"&nbsp;&#x25BE;",this.appendChild(sortfwdind),0==isscr){row_array=[],col=this.sorttable_columnindex,rows=this.sorttable_tbody.rows;for(var e=0;e<rows.length;e++)row_array[row_array.length]=[sorttable.getInnerText(rows[e].cells[col]),rows[e]];row_array.sort(this.sorttable_sortfunction),tb=this.sorttable_tbody;for(e=0;e<row_array.length;e++)tb.appendChild(row_array[e][1]);delete row_array}}))}},guessType:function(t,e,r){var o=t.tBodies[0];if(r&&(o=r.tBodies[0]),null!=o&&0==o.rows.length)return sorttable.sort_alpha;if(sortfn=sorttable.sort_alpha,null!=o){if(text=sorttable.getInnerText(o.rows[0].cells[e]),""!=text){if(text.match(/^-?[£$¤]?[\d,.]+%?$/))return sorttable.sort_numeric;if(possdate=text.match(sorttable.DATE_RE),possdate){if(first=parseInt(possdate[1]),second=parseInt(possdate[2]),12<first)return sorttable.sort_ddmm;if(12<second)return sorttable.sort_mmdd;sortfn=sorttable.sort_ddmm}}return sortfn}return sorttable.sort_alpha},getInnerText:function(t){if(!t)return"";if(hasInputs="function"==typeof t.getElementsByTagName&&t.getElementsByTagName("input").length,void 0===t.getAttribute||"function"!=typeof t.getAttribute)return"";if("input"==t.nodeName.toLowerCase()&&"checkbox"==$("#"+t.id).attr("type"))return 1==$("#"+t.id).is(":checked")?"false":"true";if(null!=t.getAttribute("sorttable_customkey"))return t.getAttribute("sorttable_customkey");if(void 0!==t.textContent&&!hasInputs)return t.textContent.replace(/^\s+|\s+$/g,"");if(void 0!==t.innerText&&!hasInputs)return t.innerText.replace(/^\s+|\s+$/g,"");if(void 0!==t.text&&!hasInputs)return t.text.replace(/^\s+|\s+$/g,"");switch(t.nodeType){case 3:if("input"==t.nodeName.toLowerCase())return t.value.replace(/^\s+|\s+$/g,"");case 4:return t.nodeValue.replace(/^\s+|\s+$/g,"");case 1:case 11:for(var e="",r=0;r<t.childNodes.length;r++)e+=sorttable.getInnerText(t.childNodes[r]);return e.replace(/^\s+|\s+$/g,"");default:return""}},reverse:function(t){if(t){newrows=[];for(var e=0;e<t.rows.length;e++)newrows[newrows.length]=t.rows[e];for(e=newrows.length-1;0<=e;e--)t.appendChild(newrows[e]);delete newrows}},sort_numeric:function(t,e){return aa=parseFloat(t[0].replace(/[^0-9.-]/g,"")),isNaN(aa)&&(aa=0),bb=parseFloat(e[0].replace(/[^0-9.-]/g,"")),isNaN(bb)&&(bb=0),aa-bb},sort_alpha:function(t,e){return t[0]==e[0]?0:t[0]<e[0]?-1:1},sort_ddmm:function(t,e){return mtch=t[0].match(sorttable.DATE_RE),y=mtch[3],m=mtch[2],d=mtch[1],1==m.length&&(m="0"+m),1==d.length&&(d="0"+d),dt1=y+m+d,mtch=e[0].match(sorttable.DATE_RE),y=mtch[3],m=mtch[2],d=mtch[1],1==m.length&&(m="0"+m),1==d.length&&(d="0"+d),dt2=y+m+d,dt1==dt2?0:dt1<dt2?-1:1},sort_mmdd:function(t,e){return mtch=t[0].match(sorttable.DATE_RE),y=mtch[3],d=mtch[2],m=mtch[1],1==m.length&&(m="0"+m),1==d.length&&(d="0"+d),dt1=y+m+d,mtch=e[0].match(sorttable.DATE_RE),y=mtch[3],d=mtch[2],m=mtch[1],1==m.length&&(m="0"+m),1==d.length&&(d="0"+d),dt2=y+m+d,dt1==dt2?0:dt1<dt2?-1:1},shaker_sort:function(t,e){for(var r=0,o=t.length-1,n=!0;n;){n=!1;for(var s=r;s<o;++s)if(0<e(t[s],t[s+1])){var a=t[s];t[s]=t[s+1],t[s+1]=a,n=!0}if(o--,!n)break;for(s=o;r<s;--s)if(e(t[s],t[s-1])<0){a=t[s];t[s]=t[s-1],t[s-1]=a,n=!0}r++}}},document.addEventListener&&document.addEventListener("DOMContentLoaded",sorttable.init,!1),/WebKit/i.test(navigator.userAgent))var _timer=setInterval(function(){/loaded|complete/.test(document.readyState)&&sorttable.init()},10);window.onload=sorttable.init,dean_addEvent.guid=1,fixEvent.preventDefault=function(){this.returnValue=!1},fixEvent.stopPropagation=function(){this.cancelBubble=!0},Array.forEach||(Array.forEach=function(t,e,r){for(var o=0;o<t.length;o++)e.call(r,t[o],o,t)}),Function.prototype.forEach=function(t,e,r){for(var o in t)void 0===this.prototype[o]&&e.call(r,t[o],o,t)},String.forEach=function(r,o,n){Array.forEach(r.split(""),function(t,e){o.call(n,t,e,r)})};var forEach=function(t,e,r){if(t){var o=Object;if(t instanceof Function)o=Function;else{if(t.forEach instanceof Function)return void t.forEach(e,r);"string"==typeof t?o=String:"number"==typeof t.length&&(o=Array)}o.forEach(t,e,r)}};
+
+*/
+
+function dean_addEvent(e, t, o) {
+    if (e.addEventListener) e.addEventListener(t, o, !1);
+    else {
+        o.$$guid || (o.$$guid = dean_addEvent.guid++), e.events || (e.events = {});
+        var n = e.events[t];
+        n || (n = e.events[t] = {}, e["on" + t] && (n[0] = e["on" + t])), n[o.$$guid] = o, e["on" + t] = handleEvent
+    }
+}
+
+function removeEvent(e, t, o) {
+    e.removeEventListener ? e.removeEventListener(t, o, !1) : e.events && e.events[t] && delete e.events[t][o.$$guid]
+}
+
+function handleEvent(e) {
+    var t = !0;
+    e = e || fixEvent(((this.ownerDocument || this.document || this).parentWindow || window).event);
+    var o = this.events[e.type];
+    for (var n in o) this.$$handleEvent = o[n], !1 === this.$$handleEvent(e) && (t = !1);
+    return t
+}
+
+function fixEvent(e) {
+    return e.preventDefault = fixEvent.preventDefault, e.stopPropagation = fixEvent.stopPropagation, e
+}
+var stIsIE = !1,
+    isscr = !1;
+if (sorttable = {
+        init: function() {
+            arguments.callee.done || (arguments.callee.done = !0, _timer && clearInterval(_timer), document.createElement && document.getElementsByTagName && (sorttable.DATE_RE = /^(\d\d?)[\/\.-](\d\d?)[\/\.-]((\d\d)?\d\d)/, forEach(document.getElementsByTagName("table"), function(e) {
+                -1 != e.className.search(/\bsortable\b/) && sorttable.makeSortable(e)
+            })))
+        },
+        makeSortable: function(e, o) {
+			var q = e;
+            if (isscr = null != o, 0 == e.getElementsByTagName("thead").length && (the = document.createElement("thead"), the.appendChild(e.rows[0]), e.insertBefore(the, e.firstChild)), null == e.tHead && (e.tHead = e.getElementsByTagName("thead")[0]), 1 == e.tHead.rows.length) {
+                sortbottomrows = [];
+                for (var t = 0; t < e.rows.length; t++) - 1 != e.rows[t].className.search(/\bsortbottom\b/) && (sortbottomrows[sortbottomrows.length] = e.rows[t]);
+                if (sortbottomrows) {
+                    for (null == e.tFoot && (tfo = document.createElement("tfoot"), e.appendChild(tfo)), t = 0; t < sortbottomrows.length; t++) tfo.appendChild(sortbottomrows[t]);
+                    delete sortbottomrows
+                }
+                for (headrow = e.tHead.rows[0].cells, t = 0; t < headrow.length; t++) headrow[t].className.match(/\bsorttable_nosort\b/) || (mtch = headrow[t].className.match(/\bsorttable_([a-z0-9]+)\b/), mtch && (override = mtch[1]), mtch && "function" == typeof sorttable["sort_" + override] ? headrow[t].sorttable_sortfunction = sorttable["sort_" + override] : headrow[t].sorttable_sortfunction = sorttable.guessType(e, t, o), headrow[t].sorttable_columnindex = t, headrow[t].sorttable_tbody = null != o ? o.tBodies[0] : e.tBodies[0], dean_addEvent(headrow[t], "click", sorttable.innerSortFunction = function(e) {
+                    if (-1 != this.className.search(/\bsorttable_sorted\b/)) return -1 == q.className.search(/\bnoautosort\b/) && sorttable.reverse(this.sorttable_tbody), this.className = this.className.replace("sorttable_sorted", "sorttable_sorted_reverse"), this.removeChild(document.getElementById("sorttable_sortfwdind")), sortrevind = document.createElement("span"), sortrevind.id = "sorttable_sortrevind", sortrevind.innerHTML = stIsIE ? '&nbsp<font face="webdings">5</font>' : "&nbsp;&#x25B4;", void this.appendChild(sortrevind);
+                    if (-1 != this.className.search(/\bsorttable_sorted_reverse\b/)) return -1 == q.className.search(/\bnoautosort\b/) && sorttable.reverse(this.sorttable_tbody), this.className = this.className.replace("sorttable_sorted_reverse", "sorttable_sorted"), this.removeChild(document.getElementById("sorttable_sortrevind")), sortfwdind = document.createElement("span"), sortfwdind.id = "sorttable_sortfwdind", sortfwdind.innerHTML = stIsIE ? '&nbsp<font face="webdings">6</font>' : "&nbsp;&#x25BE;", void this.appendChild(sortfwdind);
+                    if (theadrow = this.parentNode, forEach(theadrow.childNodes, function(e) {
+                            1 == e.nodeType && (e.className = e.className.replace("sorttable_sorted_reverse", ""), e.className = e.className.replace("sorttable_sorted", ""))
+                        }), sortfwdind = document.getElementById("sorttable_sortfwdind"), sortfwdind && sortfwdind.parentNode.removeChild(sortfwdind), sortrevind = document.getElementById("sorttable_sortrevind"), sortrevind && sortrevind.parentNode.removeChild(sortrevind), this.className += " sorttable_sorted", sortfwdind = document.createElement("span"), sortfwdind.id = "sorttable_sortfwdind", sortfwdind.innerHTML = stIsIE ? '&nbsp<font face="webdings">6</font>' : "&nbsp;&#x25BE;", this.appendChild(sortfwdind), 0 == isscr) {
+							if (-1 == q.className.search(/\bnoautosort\b/)) {
+								row_array = [], col = this.sorttable_columnindex, rows = this.sorttable_tbody.rows;
+								for (var t = 0; t < rows.length; t++) row_array[row_array.length] = [sorttable.getInnerText(rows[t].cells[col]), rows[t]];
+								for (row_array.sort(this.sorttable_sortfunction), tb = this.sorttable_tbody, t = 0; t < row_array.length; t++) tb.appendChild(row_array[t][1]);
+								delete row_array
+							}
+                    }
+                }))
+            }
+        },
+        guessType: function(e, t, o) {
+            var n = e.tBodies[0];
+            if (o && (n = o.tBodies[0]), null != n && 0 == n.rows.length) return sorttable.sort_alpha;
+            if (sortfn = sorttable.sort_alpha, null != n) {
+                if (text = sorttable.getInnerText(n.rows[0].cells[t]), "" != text) {
+                    if (text.match(/^-?[£$¤]?[\d,.]+%?$/)) return sorttable.sort_numeric;
+                    if (possdate = text.match(sorttable.DATE_RE), possdate) {
+                        if (first = parseInt(possdate[1]), second = parseInt(possdate[2]), 12 < first) return sorttable.sort_ddmm;
+                        if (12 < second) return sorttable.sort_mmdd;
+                        sortfn = sorttable.sort_ddmm
+                    }
+                }
+                return sortfn
+            }
+            return sorttable.sort_alpha
+        },
+        getInnerText: function(e) {
+            if (!e) return "";
+            if (hasInputs = "function" == typeof e.getElementsByTagName && e.getElementsByTagName("input").length, void 0 === e.getAttribute || "function" != typeof e.getAttribute) return "";
+            if ("input" == e.nodeName.toLowerCase() && "checkbox" == $("#" + e.id).attr("type")) return 1 == $("#" + e.id).is(":checked") ? "false" : "true";
+            if (null != e.getAttribute("sorttable_customkey")) return e.getAttribute("sorttable_customkey");
+            if (void 0 !== e.textContent && !hasInputs) return e.textContent.replace(/^\s+|\s+$/g, "");
+            if (void 0 !== e.innerText && !hasInputs) return e.innerText.replace(/^\s+|\s+$/g, "");
+            if (void 0 !== e.text && !hasInputs) return e.text.replace(/^\s+|\s+$/g, "");
+            switch (e.nodeType) {
+                case 3:
+                    if ("input" == e.nodeName.toLowerCase()) return e.value.replace(/^\s+|\s+$/g, "");
+                case 4:
+                    return e.nodeValue.replace(/^\s+|\s+$/g, "");
+                case 1:
+                case 11:
+                    for (var t = "", o = 0; o < e.childNodes.length; o++) t += sorttable.getInnerText(e.childNodes[o]);
+                    return t.replace(/^\s+|\s+$/g, "");
+                default:
+                    return ""
+            }
+        },
+        reverse: function(e) {
+			if (e) {
+                newrows = [];
+                for (var t = 0; t < e.rows.length; t++) newrows[newrows.length] = e.rows[t];
+                for (t = newrows.length - 1; 0 <= t; t--) e.appendChild(newrows[t]);
+                delete newrows
+            }
+			
+        },
+        sort_numeric: function(e, t) {
+            return aa = parseFloat(e[0].replace(/[^0-9.-]/g, "")), isNaN(aa) && (aa = 0), bb = parseFloat(t[0].replace(/[^0-9.-]/g, "")), isNaN(bb) && (bb = 0), aa - bb
+        },
+        sort_alpha: function(e, t) {
+            return e[0] == t[0] ? 0 : e[0] < t[0] ? -1 : 1
+        },
+        sort_ddmm: function(e, t) {
+            return mtch = e[0].match(sorttable.DATE_RE), y = mtch[3], m = mtch[2], d = mtch[1], 1 == m.length && (m = "0" + m), 1 == d.length && (d = "0" + d), dt1 = y + m + d, mtch = t[0].match(sorttable.DATE_RE), y = mtch[3], m = mtch[2], d = mtch[1], 1 == m.length && (m = "0" + m), 1 == d.length && (d = "0" + d), dt2 = y + m + d, dt1 == dt2 ? 0 : dt1 < dt2 ? -1 : 1
+        },
+        sort_mmdd: function(e, t) {
+            return mtch = e[0].match(sorttable.DATE_RE), y = mtch[3], d = mtch[2], m = mtch[1], 1 == m.length && (m = "0" + m), 1 == d.length && (d = "0" + d), dt1 = y + m + d, mtch = t[0].match(sorttable.DATE_RE), y = mtch[3], d = mtch[2], m = mtch[1], 1 == m.length && (m = "0" + m), 1 == d.length && (d = "0" + d), dt2 = y + m + d, dt1 == dt2 ? 0 : dt1 < dt2 ? -1 : 1
+        },
+        shaker_sort: function(e, t) {
+            for (var o = 0, n = e.length - 1, r = !0; r;) {
+                r = !1;
+                for (var s = o; s < n; ++s)
+                    if (0 < t(e[s], e[s + 1])) {
+                        var a = e[s];
+                        e[s] = e[s + 1], e[s + 1] = a, r = !0
+                    } if (n--, !r) break;
+                for (s = n; o < s; --s) t(e[s], e[s - 1]) < 0 && (a = e[s], e[s] = e[s - 1], e[s - 1] = a, r = !0);
+                o++
+            }
+        }
+    }, document.addEventListener && document.addEventListener("DOMContentLoaded", sorttable.init, !1), /WebKit/i.test(navigator.userAgent)) var _timer = setInterval(function() {
+    /loaded|complete/.test(document.readyState) && sorttable.init()
+}, 10);
+window.onload = sorttable.init, dean_addEvent.guid = 1, fixEvent.preventDefault = function() {
+    this.returnValue = !1
+}, fixEvent.stopPropagation = function() {
+    this.cancelBubble = !0
+}, Array.forEach || (Array.forEach = function(e, t, o) {
+    for (var n = 0; n < e.length; n++) t.call(o, e[n], n, e)
+}), Function.prototype.forEach = function(e, t, o) {
+    for (var n in e) void 0 === this.prototype[n] && t.call(o, e[n], n, e)
+}, String.forEach = function(o, n, r) {
+    Array.forEach(o.split(""), function(e, t) {
+        n.call(r, e, t, o)
+    })
+};
+var forEach = function(e, t, o) {
+    if (e) {
+        var n = Object;
+        if (e instanceof Function) n = Function;
+        else {
+            if (e.forEach instanceof Function) return void e.forEach(t, o);
+            "string" == typeof e ? n = String : "number" == typeof e.length && (n = Array)
+        }
+        n.forEach(e, t, o)
+    }
+};
 
 /*!
  * dragtable
